6139de1c2ba8e2c6710352b7b0d9e330a30b755a
Unknown <karmarra@ymail.com>
2019-01-25 22:50:07 +0300

    каркас для MVVM

---
+++ h2n_test/MainWindowViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace h2n_test
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private double _field = 0;
+        public double Field
+        {
+            get => _field;
+            set
+            {
+                if (_field == value)
+                    return;
+                _field = value;
+                OnPropertyChanged(nameof(Field));
+            }
+        }
+
+
+        #region methods
+
+        private void Action()
+        {
+
+        }
+
+        RelayCommand.RelayCommand _saveCommand;
+        public ICommand SaveCommand
+        {
+            get
+            {
+                return _saveCommand ?? (_saveCommand = new RelayCommand.RelayCommand(param => Action()));
+            }
+        }
+
+        #endregion methods
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+       
+        #endregion INotifyPropertyChanged
+    }
+}
---
+++ h2n_test/RelayCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RelayCommand
+{
+    public class RelayCommand : ICommand
+    {
+        #region Fields 
+
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        #endregion
+
+        
+        #region Ctors
+
+        public RelayCommand(Action<object> execute) : this(execute, null) { }
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        #endregion
+
+        
+        #region ICommand 
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public void Execute(object parameter) { _execute(parameter); }
+
+        #endregion ICommand
+    }
+}
