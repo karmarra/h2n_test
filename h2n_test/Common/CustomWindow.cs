fc7f917218a85f448ff36a15bc4f8c2029d97b2d
Unknown <karmarra@ymail.com>
2019-01-26 03:10:02 +0300

    кастомное окно с кнопочками
    пора спать

--- h2n_test/Common/CustomWindow.cs
+++ h2n_test/Common/CustomWindow.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace h2n_test.Common
 {
@@ -8,20 +9,31 @@ namespace h2n_test.Common
         {
         }
 
-        public void CloseButtonClick(object sender, RoutedEventArgs e)
+        public override void OnApplyTemplate()
         {
-            SystemCommands.CloseWindow(this);
+            if (GetTemplateChild("MinButton") is Button minButton)
+                minButton.Click += MinButtonOnClick;
+            if (GetTemplateChild("MaxButton") is Button maxButton)
+                maxButton.Click += MaxButtonOnClick;
+            if (GetTemplateChild("CloseButton") is Button closeButton)
+                closeButton.Click += CloseButtonOnClick;
+            base.OnApplyTemplate();
         }
 
-        public void MinButtonClick(object sender, RoutedEventArgs e)
+        private void MinButtonOnClick(object sender, RoutedEventArgs e)
         {
             SystemCommands.MinimizeWindow(this);
         }
 
-        public void MaxButtonClick(object sender, RoutedEventArgs e)
+        private void MaxButtonOnClick(object sender, RoutedEventArgs e)
         {
             if (WindowState == WindowState.Maximized) SystemCommands.RestoreWindow(this);
             else SystemCommands.MaximizeWindow(this);
+        }
+
+        private void CloseButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
         }
     }
 }
