0af2e4a7943fd9f72a21f3b93b053f5bf1468d29
Unknown <karmarra@ymail.com>
2019-01-29 23:04:39 +0300

    обводка вокруг кнопок, если можно её так назвать))

--- h2n_test/MainWindow.xaml.cs
+++ h2n_test/MainWindow.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using h2n_test.Common;
 
@@ -22,6 +13,27 @@ namespace h2n_test
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Brd_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            var btn = (Border)sender;
+            var gradient = btn.Background as RadialGradientBrush;
+            if (gradient == null)
+                return;
+            gradient.Opacity = 1;
+            Point pt = Mouse.GetPosition(btn);
+            gradient.GradientOrigin = new Point(pt.X / btn.ActualWidth, pt.Y / btn.ActualHeight);
+            gradient.Center = gradient.GradientOrigin;
+        }
+
+        private void Brd_OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            var btn = (Border)sender;
+            var gradient = btn.Background as RadialGradientBrush;
+            if (gradient == null)
+                return;
+            gradient.Opacity = 0;
         }
     }
 }
